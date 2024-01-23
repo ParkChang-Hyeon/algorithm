2a13040534cc77d23856a1c25578dea3bf2b7549
ParkChang-Hyeon <154569535+ParkChang-Hyeon@users.noreply.github.com>
2024-01-23 09:29:02 +0900

    [level 1] Title: 평균 구하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cs
@@ -0,0 +1,13 @@
+public class Solution 
+{
+    public double solution(int[] arr) 
+    {
+        double avg = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            avg += arr[i];
+        }
+        double answer = avg / arr.Length;
+        return answer;
+    }
+}
